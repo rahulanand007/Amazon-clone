f3e442e594e00bf383f0203a521cbbf853787795
rahulanand007 <rahul03061998@gmail.com>
2022-05-22 17:35:31 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import '../styling/Navbar.css'
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img className='navbar__logo' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'></img>
+
+        <div className="navbar__search">
+            <input className='navbar__searchInput' type="text"/>
+            <SearchIcon className='navbar__searchIcon'/>
+        </div>
+
+        <div className="navbar__nav">
+            <div className="navbar__option">
+                <span className="navbar__optionLineOne">Hello Guest</span>
+                <span className="navbar__optionLineTwo">Sign In</span>
+            </div>
+            <div className="navbar__option">
+                <span className="navbar__optionLineOne">Returns</span>
+                <span className="navbar__optionLineTwo">& Orders</span>
+            </div>
+            <div className="navbar__option">
+                <span className="navbar__optionLineOne">Your</span>
+                <span className="navbar__optionLineTwo">Prime</span>
+            </div>
+            <div className="navbar__option">
+                <ShoppingBasketIcon className='shoppingBasket'/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB7WFI0ucqbJty5cFyYFuhkAuqp7DtkU8A",
+    authDomain: "clone-da907.firebaseapp.com",
+    projectId: "clone-da907",
+    storageBucket: "clone-da907.appspot.com",
+    messagingSenderId: "181929264072",
+    appId: "1:181929264072:web:f4343b8d50098c4a9df6fd",
+    measurementId: "G-GCXTEK1QSF"
+  };
